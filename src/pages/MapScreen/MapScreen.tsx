2e927687f37af7d27e6bbb47d24edae01019968c
Stanley Sathler <stanleysathlerpinto@gmail.com>
2020-06-16 16:36:45 -0300

    refac: add model `cases`

--- src/pages/MapScreen/MapScreen.tsx
+++ src/pages/MapScreen/MapScreen.tsx
@@ -3,6 +3,7 @@ import { GoogleMap, LoadScript, Marker, HeatmapLayer } from '@react-google-maps/
 import * as api from '../../utils/api'
 import { listen as listenToStores, Store } from '../../models/stores'
 import { listen as listenToUsers, User } from '../../models/users'
+import { fetch as fetchCases, Case } from '../../models/cases';
 
 const style = {
   width: '100%',
@@ -16,7 +17,7 @@ const mapCenter = {
 
 const libraries = ['visualization']
 
-const buildHeatmapData = (cases: api.Case[]) => (
+const buildHeatmapData = (cases: Case[]) => (
   cases.map(c => ({
     // @ts-ignore
     location: new google.maps.LatLng(c.location.lat, c.location.lng),
@@ -24,12 +25,17 @@ const buildHeatmapData = (cases: api.Case[]) => (
   }))
 )
 
+type HeatmapLayerOptions = google.maps.visualization.HeatmapLayerOptions
+type HeatmapOptions = Omit<HeatmapLayerOptions, 'data'>
+type HeatmapItem = google.maps.visualization.WeightedLocation
+
 export default function MapScreen() {
   const [stores, updateStores] = useState<Store[]>([])
   const [hospitals, updateHospitals] = useState<api.Hospital[]>([])
   const [sickUsers, updateUsers] = useState<User[]>([])
-  const [cases, updateCases] = useState<api.Case[]>([])
-  const [heatmap, updateHeatmap] = useState<google.maps.visualization.HeatmapLayerOptions>()
+  const [cases, updateCases] = useState<Case[]>([])
+  const [heatOptions] = useState<HeatmapOptions>({ radius: 200, opacity: 0.25 })
+  const [heatData, updateHeatData] = useState<HeatmapItem[]>([])
 
   useEffect(() => {
     listenToStores(stores => {
@@ -44,8 +50,7 @@ export default function MapScreen() {
   }, [])
 
   useEffect(() => {
-    api.get('/cases').then(data => {
-      const cases = (data as unknown as { data: api.Case[] }).data
+    fetchCases(cases => {
       updateCases(cases)
     })
   }, [])
@@ -57,19 +62,15 @@ export default function MapScreen() {
     })
   }, [])
 
-  const handleMapsScriptLoaded = useCallback(() => {
-    updateHeatmap({
-      radius: 200,
-      data: buildHeatmapData(cases),
-      opacity: 0.25
-    })
+  useEffect(() => {
+    if (typeof window.google !== 'undefined')
+      updateHeatData(buildHeatmapData(cases))
   }, [cases])
 
   return (
     <LoadScript
       googleMapsApiKey={process.env.GOOGLE_API_KEY}
       libraries={libraries}
-      onLoad={handleMapsScriptLoaded}
     >
       <GoogleMap
         mapContainerStyle={style}
@@ -81,8 +82,8 @@ export default function MapScreen() {
       >
         {/* Confirmed cases heatmap. */}
         <HeatmapLayer
-          options={heatmap}
-          data={[]}
+          options={heatOptions as HeatmapLayerOptions}
+          data={heatData}
         />
 
         {/* Render stores. */}
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,20 +9,6 @@ export type Hospital = {
   location: { lat: number, lng: number };
 };
 
-export type Case = {
-  id: number;
-  neighborhood: string;
-  location: { lat: number, lng: number };
-  serious: number;
-  nonSerious: number;
-  deaths: number;
-};
-
-const mockedCases: Case[] = [
-  { id: 1, neighborhood: 'Estoril', location: location(-19.961755, -43.961543), serious: 5, nonSerious: 7, deaths: 0 },
-  { id: 2, neighborhood: 'Buritis', location: location(-19.975713, -43.968317), serious: 12, nonSerious: 10, deaths: 2 },
-];
-
 const mockedHospitals: Hospital[] = [
   { id: 1, name: 'Hermes Pardini', location: location(-19.960919, -43.969963) }
 ]
@@ -30,9 +16,7 @@ const mockedHospitals: Hospital[] = [
 export const get = (route: string) => {
   // return axios.get(process.env.API_URL + route)
   return new Promise((res) => {
-    if (route === '/cases')
-      return res({ data: mockedCases })
-    else if (route === '/hospitals')
+    if (route === '/hospitals')
       return res({ data: mockedHospitals })
   })
 }
