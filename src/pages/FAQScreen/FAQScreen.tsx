0cee9a7961434b7670c401f1ba511f3573509b86
Stanley Sathler <stanleysathlerpinto@gmail.com>
2020-06-17 15:18:02 -0300

    chore: fix lint issues

--- src/pages/FAQScreen/FAQScreen.tsx
+++ src/pages/FAQScreen/FAQScreen.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Container from '../../components/Container/Container';
-import Chat from '../../components/Chat/Chat';
+import React from 'react'
+import Container from '../../components/Container/Container'
+import Chat from '../../components/Chat/Chat'
 
 export default function FAQScreen() {
   return (
     <Container>
       <Chat />
     </Container>
-  );
-};
+  )
+}
